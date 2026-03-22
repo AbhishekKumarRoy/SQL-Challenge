bb4c336eb1d2510421be331869a17e26a35c932a
Abhishek Roy <57017397+AbhishekKumarRoy@users.noreply.github.com>
2026-03-23 01:57:59 +0530

    Update Solution.sql

--- Case Study #1 - Danny's Diner /Solution.sql
+++ Case Study #1 - Danny's Diner /Solution.sql
@@ -3,18 +3,24 @@
    --------------------*/
 
 -- 1. What is the total amount each customer spent at the restaurant?
-SELECT s.customer_id, SUM(m.price) AS Total_sales
+SELECT 
+   s.customer_id, 
+   SUM(m.price) AS Total_sales
 FROM sales s
 JOIN menu m ON s.product_id = m.product_id
 GROUP BY s.customer_id;
 
 -- 2. How many days has each customer visited the restaurant?
-SELECT customer_id, COUNT(DISTINCT order_date) AS Time_visited
+SELECT 
+   customer_id, 
+   COUNT(DISTINCT order_date) AS Time_visited
 FROM sales
 GROUP BY customer_id;
 
 -- 3. What was the first item from the menu purchased by each customer?
-SELECT DISTINCT s.customer_id, m.product_name
+SELECT 
+   DISTINCT s.customer_id, 
+   m.product_name
 FROM sales s
 JOIN menu m ON s.product_id = m.product_id
 WHERE s.order_date IN (
@@ -25,7 +31,9 @@ WHERE s.order_date IN (
 
 
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
-SELECT m.product_name, COUNT(m.product_name) AS purchase_count
+SELECT 
+   m.product_name, 
+   COUNT(m.product_name) AS purchase_count
 FROM sales s
 JOIN menu m ON s.product_id = m.product_id
 GROUP BY m.product_name
@@ -35,23 +43,37 @@ LIMIT 1;
 
 -- 5. Which item was the most popular for each customer?
 WITH customer_purchases AS (
-    SELECT s.customer_id, m.product_name, COUNT(*) AS product_count
+    SELECT 
+      s.customer_id, 
+      m.product_name, 
+      COUNT(*) AS product_count
     FROM sales s
     JOIN menu m ON s.product_id = m.product_id
-    GROUP BY s.customer_id, m.product_name
+    GROUP BY 
+      s.customer_id, 
+      m.product_name
 ),
 max_purchases AS (
-    SELECT customer_id, MAX(product_count) AS max_count
+    SELECT 
+      customer_id, 
+      MAX(product_count) AS max_count
     FROM customer_purchases
     GROUP BY customer_id
 )
-SELECT cp.customer_id, cp.product_name, cp.product_count
+SELECT 
+   cp.customer_id, 
+   cp.product_name, 
+   cp.product_count
 FROM customer_purchases cp
-JOIN max_purchases mp ON cp.customer_id = mp.customer_id AND cp.product_count = mp.max_count;
+JOIN max_purchases mp ON cp.customer_id = mp.customer_id 
+   AND cp.product_count = mp.max_count;
 
 
 -- 6. Which item was purchased first by the customer after they became a member?
-SELECT s.customer_id, s.order_date AS first_order, me.product_name
+SELECT 
+   s.customer_id, 
+   s.order_date AS first_order, 
+   me.product_name
 FROM sales s
 JOIN members m ON s.customer_id = m.customer_id
 JOIN menu me ON s.product_id = me.product_id
@@ -66,7 +88,10 @@ WHERE
 
 
 -- 7. Which item was purchased just before the customer became a member?
-SELECT s.customer_id, s.order_date AS first_order, me.product_name
+SELECT 
+   s.customer_id, 
+   s.order_date AS first_order, 
+   me.product_name
 FROM sales s
 JOIN members m ON s.customer_id = m.customer_id
 JOIN menu me ON s.product_id = me.product_id
@@ -81,7 +106,10 @@ WHERE
 
 
 -- 8. What is the total items and amount spent for each member before they became a member?
-SELECT s.customer_id, COUNT(me.product_name) AS total_items, SUM(me.price) AS total_amount_spent
+SELECT 
+   s.customer_id, 
+   COUNT(me.product_name) AS total_items, 
+   SUM(me.price) AS total_amount_spent
 FROM sales s
 JOIN members m ON s.customer_id = m.customer_id
 JOIN menu me ON s.product_id = me.product_id 
